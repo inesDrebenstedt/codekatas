bf0f1d823090b008dca813286349e497bcc30cbc
inesDrebenstedt <ines.drebenstedt@googlemail.com>
2023-04-19 12:52:32 +0200

    added class method.

--- Coding challenges/src/FindingTheNumbers/NumbersFinder.java
+++ Coding challenges/src/FindingTheNumbers/NumbersFinder.java
@@ -1,4 +1,9 @@
 package FindingTheNumbers;
 
 public class NumbersFinder {
+
+    public static int lookUpTwins(int[] numArray){
+        int numberOfNumbersWithoutTwin = 0;
+        return numberOfNumbersWithoutTwin;
+    }
 }
